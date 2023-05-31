a3f052f49b0af541a4958765046b568c2770d438
Maks Subochev <maxim.subochev@yandex.ru>
2023-05-31 23:51:59 +0300

    feat: добавить меню для мобильной версии

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,67 @@
 import { Link, Routes, Route } from "react-router-dom";
 import logo from "../images/header__logo.svg";
+import { useState } from "react";
 
-function Header({email, onExit}) {
+function Header({ email, onExit }) {
+
+const [isShow, setIsShow] = useState(false);
+
+const handleShow = () => {
+  setIsShow(!isShow);
+}
+
   return (
-    <header className="header">
-      <a href="https://goplomah.github.io/react-mesto-auth/" className="header__link">
+    <header className={`header ${isShow && 'header_show'}`}>
+      <a
+        href="https://goplomah.github.io/react-mesto-auth/"
+        className="header__link"
+      >
         <img src={logo} alt="логотип сайта место." className="header__logo" />
       </a>
-      <nav className="header__nav">
-        <p className="header__email">{email}</p>
         <Routes>
-          <Route path='/sign-in' element={<Link className="header__link" to='/sign-up'>Регистрация</Link>}/>
-          <Route path='/sign-up' element={<Link className="header__link" to='/sign-in'>Вход</Link>}/>
-          <Route path='/' element={<Link className="header__link" to='/sign-in' onClick={onExit}>Выход</Link>}/>
-          <Route path='*' element={<Link className="header__link" to='/'>На главную</Link>}/>
-        </Routes>
+          <Route
+            path="/sign-in"
+            element={
+              <Link className="header__link" to="/sign-up">
+                Регистрация
+              </Link>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <Link className="header__link" to="/sign-in">
+                Вход
+              </Link>
+            }
+          />
+        <Route
+          path="*"
+          element={
+            <Link className="header__link" to="/">
+              На главную
+            </Link>
+          }
+        />
+          <Route
+            path="/"
+            element={
+              <>
+      <nav className={`header__nav ${isShow && 'header__nav_show'}`}>
+        <p className="header__email">{email}</p>
+              <Link className="header__link" to="/sign-in" onClick={onExit}>
+                Выход
+              </Link>
       </nav>
+        <button className="menu" onClick={handleShow}>
+          <span className={`menu__line ${isShow && 'menu__line-1'}`}></span>
+          <span className={`menu__line ${isShow && 'menu__line-2'}`}></span>
+          <span className={`menu__line ${isShow && 'menu__line-3'}`}></span>
+        </button>
+        </>
+            }
+          />
+        </Routes>
     </header>
   );
 }
